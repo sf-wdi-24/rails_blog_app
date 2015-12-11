7234105e0c9af9757f2f29f3adbbdbd52bec0c72
cameronjacoby <cameron.jacoby@gmail.com>
2015-12-11 15:48:47 -0800

    implement signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root "users#index"
-  resources :users
+  resources :users, except: [:new]
+
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  resources :sessions, only: [:create, :destroy]
 
 end
