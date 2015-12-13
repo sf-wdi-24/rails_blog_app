19910df6ce256a578259adb78cf994a96bcbc952
cameronjacoby <cameron.jacoby@gmail.com>
2015-12-13 09:19:14 -0800

    finish adding user controller specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    email FFaker::Internet.email
-    password FFaker::Lorem.words(2).join
+    email { FFaker::Internet.email }
+    password { FFaker::Lorem.words(5).join }
   end
 end
