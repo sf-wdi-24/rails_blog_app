98d056d4d033b14ae56115b12e135303feb71fbe
cameronjacoby <cameron.jacoby@gmail.com>
2015-12-12 23:36:25 -0800

    remove comments index and show; set up before_filter on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,13 @@
 class CommentsController < ApplicationController
-
-  def index
-  end
+  before_filter :set_post
+  before_filter :set_comment, except: [:new, :create]
+  before_filter :authorize
 
   def new
+    @comment = Comment.new
   end
 
   def create
-  end
-
-  def show
   end
 
   def edit
@@ -20,5 +18,21 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+    def set_post
+      post_id = params[:post_id]
+      @post = Post.find_by_id(post_id)
+    end
+
+    def set_comment
+      comment_id = params[:id]
+      @comment = Comment.find_by_id(comment_id)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "posts#index"
   resources :posts do
-    resources :comments
+    resources :comments, except: [:index, :show]
   end
 
   get "/signup", to: "users#new"
