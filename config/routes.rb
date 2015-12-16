9f3d41ea676365f61ace647d012b9e42cddf2d81
Cynthia Salinas <csalinas123@gmail.com>
2015-12-15 22:10:50 -0800

    add new migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   root "users#index"
-  resources :users
 
+  # renders the form on the page
+  get '/signup'
+  # receives the form and creates a user in the db
+  post '/users'
+
+  resources :users
 end
