5f8ce49ce2e9902d13df801be42effdc56ef3b86
Cynthia Salinas <csalinas123@gmail.com>
2015-12-15 18:51:23 -0800

    add index, new and create pages and routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+    # getting all users and saving to instance variable
+    @users = User.all
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:email, :password)
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+    end
+  end
+
+  def show
+    user_id = params[:id]
+    @user = User.find(user_id)
+    render :show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
