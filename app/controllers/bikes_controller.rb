0549c1d862632eccbf906db3db221a7d79268a21
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-12-15 17:38:58 -0800

    got new index page up

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,8 +1,27 @@
 class BikesController < ApplicationController
 
+	def new
+		@bike = Bike.new
+		render :new
+	end
+
 	def index
 		@bikes = Bike.all
 
 		render :index
+	end
+
+	def create
+		#create params
+		bike_params = params.require(:bike).permit(:name, :description)
+
+		#create a new bike
+		bike = Bike. new(bike_params)
+
+		# if bike saves, redirect to route that displays all bikes
+		if bike.save
+			redirect_to bike_path
+			#redirect to /bikes
+		end
 	end
 end
