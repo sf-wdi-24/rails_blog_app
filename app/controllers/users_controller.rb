4df4fdc9f5d086f679a14f1f947f85e4426b7b4d
Cynthia Salinas <csalinas123@gmail.com>
2015-12-15 23:58:11 -0800

    modify view pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,3 +33,10 @@ class UsersController < ApplicationController
   def destroy
   end
 end
+
+
+private
+
+  def user_params
+    params.require(:user).permit( :email, :password, :password_confirmation)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
   root "users#index"
-
-  # renders the form on the page
-  get '/signup'
-  # receives the form and creates a user in the db
-  post '/users'
-
   resources :users
 end
