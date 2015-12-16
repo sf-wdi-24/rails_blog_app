02cec90d4e54fb344c2b127e2349e9b9a3a831de
stanleyyork <stanleyyork@gmail.com>
2015-12-15 16:21:39 -0800

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		user = User.find_by_email(params['/login']['email'])
+		if user && user.authenticate(params['/login']['password'])
+			if params[:remember_me]
+			  cookies.permanent[:auth_token] = user.auth_token
+			else
+			  cookies[:auth_token] = user.auth_token
+			end
+			redirect_to root_url, :notice => "Logged in!"
+		else
+			flash[:error] = user.errors.full_messages.join(", ")
+			render "new"
+		end
+	end
+
+	def destroy
+		cookies.delete(:auth_token)
+		redirect_to root_url, :notice => "Logged out!"
+	end
+end
