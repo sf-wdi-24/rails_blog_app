f39a12d68a9c4858c6c28dc4b19189dd1fe4c1f0
tkhuynh <thequest2311@yahoo.com>
2015-12-15 23:57:49 -0800

    flash msg won't show in view, edit won't work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,37 +2,20 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-	end
+  end
 
   def new
     @user = User.new
   end
 
   def create
-		@user = User.new(user_params)
-		if @user.save
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:error] = @user.errors.full_messages.join(", ") 
+      flash[:error] = @user.errors.full_messages.join(', ')
       redirect_to new_user_path
-    end
-	end
-
-  def edit
-    user_id = params[:id]
-    @user = User.find_by_id(user_id)
-    render :edit
-  end
-
-  def update
-    user_id = params[:id]
-    @user = user.find_by_id(user_id)
-    @user.update_attributes(user_params)
-    if user.errors.any?
-      flash[:error] = @user.errors.full_messages.join(", ") 
-      redirect_to new_user_path
-    else
-      redirect_to edit_user_path(@user)
     end
   end
 
@@ -41,9 +24,26 @@ class UsersController < ApplicationController
     @user = User.find_by_id(user_id)
   end
 
+  def edit
+    user_id = params[:id]
+    @user = User.find_by_id(user_id)
+  end
+
+  def update
+    user_id = params[:id]
+    @user = User.find_by_id(user_id)
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      flash[:error] = @user.errors.full_messages.join(', ')
+      redirect_to edit_user_path(@user)
+    end
+  end
+
   def destroy
     user_id = params[:id]
     User.find_by_id(user_id).destroy
+    session[:user_id] = nil
     redirect_to root_path
   end
 
