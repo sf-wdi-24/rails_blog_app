df5ad281973170ff1b2efc04e0741d1be8595e15
tkhuynh <thequest2311@yahoo.com>
2015-12-16 17:34:32 -0800

    prevent current user to create new acc, login again, edit and delete other's account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,25 @@
 class SessionsController < ApplicationController
   
   def new
+    #prevent current user to see login view
+    if current_user
+      redirect_to user_path(current_user)
+    end
 	end
 	
 	def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to users_path
     else
-      redirect_to '/login'
+      redirect_to login_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to login_path
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,28 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    #prevent current user to see signup view
+    #redirect to its profile page
+    if current_user
+      redirect_to user_path(current_user)
+    else 
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      flash[:error] = @user.errors.full_messages.join(', ')
-      redirect_to new_user_path
+    #prevent current user to create new account
+    if current_user
+      redirect_to user_path(current_user)
+    else 
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:error] = @user.errors.full_messages.join(', ')
+        redirect_to new_user_path
+      end
     end
   end
 
@@ -25,26 +36,42 @@ class UsersController < ApplicationController
   end
 
   def edit
+    # prevent current user to see other use edit user view
     user_id = params[:id]
     @user = User.find_by_id(user_id)
+    unless current_user == @user # unless the user edit view belong to current user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     user_id = params[:id]
     @user = User.find_by_id(user_id)
-    if @user.update_attributes(user_params)
-      redirect_to user_path(@user)
-    else
-      flash[:error] = @user.errors.full_messages.join(', ')
-      redirect_to edit_user_path(@user)
+    # prevent current user to edit other user info
+    if current_user == @user
+      if @user.update_attributes(user_params)
+        redirect_to user_path(@user)
+      else
+        flash[:error] = @user.errors.full_messages.join(', ')
+        redirect_to edit_user_path(@user)
+      end
+    else 
+      flash[:notice] = "You cannot edit other user's account!"
+      redirect_to user_path(current_user)
     end
   end
 
   def destroy
     user_id = params[:id]
-    User.find_by_id(user_id).destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    @user = User.find_by_id(user_id)
+    if current_user == @user
+      @user.destroy
+      session[:user_id] = nil
+      redirect_to root_path
+    else
+      flash[:notice] = "You cannot delete other user's account!"
+      redirect_to user_path(@user)
+    end
   end
 
   private
