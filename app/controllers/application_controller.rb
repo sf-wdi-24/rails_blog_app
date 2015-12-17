2a0cff173f729dfc71946a91421fe717473fccd4
stanleyyork <stanleyyork@gmail.com>
2015-12-16 16:58:49 -0800

    fixed some indenting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
-  
+
   def current_user
     current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
   end
-  
+
   helper_method :current_user
 
   def authorize
@@ -13,9 +13,9 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user_logged_in
-  	if current_user
-  		redirect_to '/'
-  	end
+		if current_user
+    	redirect_to '/'
+    end
   end
 
 end
