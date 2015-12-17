638a468d84b11adbc50eb823ed93f3cff69aa8ca
YOUR_GITHUB_USERNAME <dave.sloan@gmail.com>
2015-12-16 18:54:26 -0800

    got it working again

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,10 @@ Rails.application.routes.draw do
 
   resources :bikes
 
-# route sends request to our url
-  root to: 'bikes#user'
-
 #these routes are for showing users
   get '/login' => 'session#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
-  get '/new' => 'bikes#new'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
