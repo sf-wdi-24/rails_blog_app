7beedfbc740f4485ce5f02e5c015b455a2ec879c
mrockway <mrockway@gmail.com>
2015-12-17 21:05:03 -0800

    updated index page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
 
-	validates :title, presence: true, length: {minimum: 3}
-	validates :content, presence: true, length: {minimum: 10}
+	validates :title, presence: true, length: {minimum: 3}, format: { with: /dino/i,
+    				 message: "Please use the word dinosaurs" }
+	validates :content, presence: true, length: {minimum: 10}, format: { with: /dino/i,
+    				 message: "Please use the word dinosaurs" }
 
 	belongs_to :user
 
